28b234bb9c55ca918fb09fd8b9fd625c7074d276
Eugen Eisler <eoeisler@gmail.com>
2017-02-14 11:23:17 +0100

    Introduce DesignKotlinContextFactory

--- ee-slides_des/src/main/kotlin/ee/slides/dsl/SlidesGenerator.kt
+++ ee-slides_des/src/main/kotlin/ee/slides/dsl/SlidesGenerator.kt
@@ -1,8 +1,9 @@
 package ee.slides.dsl
 import ee.design.gen.DesignKotlinGenerator
+import ee.lang.integ.dPath
 import ee.lang.integ.eePath
 
 fun main(args: Array<String>) {
     val generator = DesignKotlinGenerator(Slides)
-    generator.generate(eePath)
+    generator.generate(dPath)
 }
