9f43a18c64c6592b8c0c4a8cf2762f78dcf81e07
Eugen Eisler <eoeisler@gmail.com>
2023-02-16 11:55:14 +0100

    Working state

--- ee-slides_des/src/main/kotlin/ee/slides/dsl/SlidesGenerator.kt
+++
@@ -1,9 +0,0 @@
-package ee.slides.dsl
-
-import ee.design.gen.kt.DesignKotlinGenerator
-import ee.lang.integ.dPath
-
-fun main(args: Array<String>) {
-    val generator = DesignKotlinGenerator(Slides)
-    generator.generate(dPath)
-}
--- ee-slides_fx/src/main/kotlin/ee/slides/fx/SlidesApp.kt
+++
@@ -1,6 +0,0 @@
-package ee.slides.fx
-
-import ee.slides.fx.view.PresentationView
-import tornadofx.App
-
-class SlidesApp : App(PresentationView::class, Styles::class)
